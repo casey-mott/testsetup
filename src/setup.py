4bf708c49ba48c8230b206deed202d51bb92ac3e
casey-mott <70348469+casey-mott@users.noreply.github.com>
2022-08-04 11:13:32 -0400

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='My First Setup File v1.2',
-    version='1.2',
+    name='info_assurance_package',
+    version='1.3',
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'my_start=my_package.__main__:main',
-        ]
-    },
     install_requires=[
-        'numpy>1.18',
-        'django<3.0'
-    ],
-    extras_require={
-        'opt1': ['serial'],
-        'opt2': ['pyserial'],
-    }
+        'google-api-core==1.31.3',
+        'google-auth==1.35.0',
+        'google-cloud-bigquery==1.28.0',
+        'google-cloud-core==1.7.2',
+        'google-resumable-media==1.3.3',
+        'googleapis-common-protos==1.52.0',
+        'numpy==1.20.1',
+        'pandas==1.2.4',
+        'pytz==2021.1',
+        'PyYAML==6.0'
+    ]
 )
