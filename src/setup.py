4468c53e170171f5db9c644d60a18ffd3781dde5
casey-mott <70348469+casey-mott@users.noreply.github.com>
2022-08-04 10:55:37 -0400

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=[
-        'google-api-core',
+        'google-api-core==1.31.3',
         'google-auth==1.35.0',
         'google-cloud-bigquery==1.28.0',
         'google-cloud-core==1.7.2',
@@ -13,7 +13,7 @@ setup(
         'googleapis-common-protos==1.52.0',
         'numpy==1.20.1',
         'pandas==1.2.4',
-        'protobuf==4.21.4',
+        'protobuf==3.18.0',
         'pytz==2021.1',
         'PyYAML==6.0'
     ]
