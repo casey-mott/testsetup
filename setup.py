9efa510401ca3a442df773ac444807ca53363115
casey-mott <70348469+casey-mott@users.noreply.github.com>
2022-08-04 09:51:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='testingname',
     version='1.0',
-    packages=find_packages(where='package')
+    packages=find_packages(where='my_package')
 )
